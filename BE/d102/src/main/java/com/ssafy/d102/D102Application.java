47cbe4848a45986c0fd8527cc83140a01e155524
박성환 <cyctjdghks@naver.com>
2023-03-09 13:57:29 +0900

    feat: be preferences

---
+++ BE/d102/src/main/java/com/ssafy/d102/D102Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.d102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D102Application.class, args);
+	}
+
+}
---
+++ BE/d102/src/test/java/com/ssafy/d102/D102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.d102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
