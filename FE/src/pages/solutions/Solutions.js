74bb03b93b21521ac1af83d15fd417dc8c05890b
양서정 <seojeong4560@daum.net>
2023-03-17 17:48:44 +0900

    feat: fe contact page

--- FE/src/pages/solutions/Solutions.js
+++ FE/src/pages/solutions/Solutions.js
@@ -1,7 +1,14 @@
+import classes from "./Solution.module.css";
+
 const Solutions = () =>{
   return(
     <div>
-      Solutions Page
+      <div className={classes.titleBox}>
+        <p>developed by AIVE</p>
+        <hr className={classes.hr}/>
+        <h1>Automatic Attendance and Tracking System</h1>
+        <p>AATS, 자동 출결 및 추적 시스템</p>
+      </div>
     </div>
   )
 }
