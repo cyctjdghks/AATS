4a202fcf47a89b152e5e2ae930abbced8bb71684
Byeongsoo Lee <byoung1997s4@gmaail.com>
2023-03-20 17:53:58 +0900

    feat: fe summary

--- FE/src/pages/mypage/components/calendar/Calendar.js
+++ FE/src/pages/mypage/components/calendar/Calendar.js
@@ -10,22 +10,48 @@ import classes from "./Calendar.module.css";
 const Test = () => {
   const startData = useSelector((state) => state.start.timeList);
   const endData = useSelector((state) => state.end.timeList);
-  console.log(startData);
   const localizer = momentLocalizer(moment);
 
-  const eventPropGetter = (event, start, end, isSelected) => {
+  const eventPropGetter = (event) => {
     let backgroundColor = "";
-    if (event.type === "start") {
-      backgroundColor = "red";
-    } else if (event.type === "end") {
-      backgroundColor = "blue";
-    }
+    let color = "";
+    let width = "80%";
+    let textAlign = "center";
+    let marginLeft = "auto";
+    let marginRight = "auto";
+    let marginTop = "5px";
+    let fontWeight = "500";
+    let cursor = "default";
+    if (event.type === "지각") {
+      backgroundColor = "#FF3232";
+    } else if (event.type === "퇴근") {
+      backgroundColor = "#D8DCDF";
+      color = "black";
+    } else if (event.type === "정상출근") {
+      backgroundColor = "#58DE29";
+    } else if (event.type === "조퇴") {
+      backgroundColor = "#FF3232";
+    } 
     return {
       style: {
         backgroundColor,
+        color,
+        width,
+        textAlign,
+        marginLeft,
+        marginRight,
+        marginTop,
+        fontWeight,
+        cursor,
       },
     };
   };
+
+  const stringToInt = (data) => {
+    const ans = parseInt(data, 10);
+    return ans;
+  };
+
 
   const test = () => {
     const tmp = [];
@@ -33,16 +59,16 @@ const Test = () => {
       tmp.push({
         start: new Date(startData[i]),
         end: new Date(startData[i]),
-        type: "start",
-        title: startData[i].slice(11, 16),
+        type: stringToInt(startData[i].slice(11, 13)) > 9 ? "지각" : "정상출근",
+        title: "출근 " + moment(new Date(startData[i])).format("HH:mm"),
       });
     }
     for (let i = 0; i < endData.length; i++) {
       tmp.push({
         start: new Date(endData[i]),
         end: new Date(endData[i]),
-        type: "end",
-        title: endData[i].slice(11, 16),
+        type: stringToInt(endData[i].slice(11, 13)) < 18 ? "조퇴" : "퇴근",
+        title: "퇴근 " + moment(new Date(endData[i])).format("HH:mm"),
       });
     }
     return tmp;
