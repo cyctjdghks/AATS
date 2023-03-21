a18352bcc21b0bed58d839b619e53fe6261fade3
jaehyeon park <jhp1276@naver.com>
2023-03-22 03:09:01 +0900

    fix:be yml change

--- BE/d102/src/main/java/com/ssafy/d102/structure/config/WebSecurityConfig.java
+++ BE/d102/src/main/java/com/ssafy/d102/structure/config/WebSecurityConfig.java
@@ -12,16 +12,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
                 .headers().frameOptions().disable();
+    }
+
+    // Password 암호화
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
  }
