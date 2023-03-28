f2ea7c36ba5afaecd89f690a1b9f3ae2f13959e4
양서정 <seojeong4560@daum.net>
2023-03-28 17:54:37 +0900

    feat/fe/226

--- FE/src/pages/auth/login/type/Type.js
+++ FE/src/pages/auth/login/type/Type.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import CameraOutdoorIcon from "@mui/icons-material/CameraOutdoor";
 import PersonIcon from "@mui/icons-material/Person";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import classes from "./Type.module.css";
 
@@ -26,45 +26,46 @@ const Type = () => {
 
   return (
     <div className={classes.pageBox}>
-      <h3>로그인</h3>
-      <p className={classes.pageText}>
-        당신의 유형을 선택해 로그인해 주세요
-      </p>
-
-      <div className={classes.typeBox}>
-        <div className={classes.typeOne}>
-          <CameraOutdoorIcon
-            onClick={toOrganizationLogin}
-            sx={{ fontSize: 140 }}
-            className={classes.organization}
-            // color="primary"
-          />
-          <p>기관</p>
-        </div>
-        <div className={classes.typeTwo}>
-          <PersonIcon
-            onClick={toUserLogin}
-            sx={{ fontSize: 140 }}
-            className={classes.user}
-            // color="primary"
-          />
-          <p>회원</p>
-        </div>
-        <div className={classes.typeThree}>
-          <DirectionsRunIcon
-            onClick={toWorkerLogin}
-            sx={{ fontSize: 140 }}
-            className={classes.worker}
-            // color="primary"
-          />
-          <p>근무자</p>
-        </div>
+      <div className={classes.topBox}>
+        <br />
       </div>
-      <div className={classes.signupBox}>
-        <p>회원가입 하러 가기</p>
-        <ExitToAppIcon
-        className={classes.signupImg}
-        />
+      <div className={classes.loginBox}>
+        <h3>로그인</h3>
+        <p className={classes.pageText}>당신의 유형을 선택해 로그인해 주세요</p>
+
+        <div className={classes.typeBox}>
+          <div className={classes.typeOne}>
+            <CameraOutdoorIcon
+              onClick={toOrganizationLogin}
+              sx={{ fontSize: 140 }}
+              className={classes.organization}
+              // color="primary"
+            />
+            <p>기관</p>
+          </div>
+          <div className={classes.typeTwo}>
+            <PersonIcon
+              onClick={toUserLogin}
+              sx={{ fontSize: 140 }}
+              className={classes.user}
+              // color="primary"
+            />
+            <p>회원</p>
+          </div>
+          <div className={classes.typeThree}>
+            <DirectionsRunIcon
+              onClick={toWorkerLogin}
+              sx={{ fontSize: 140 }}
+              className={classes.worker}
+              // color="primary"
+            />
+            <p>근무자</p>
+          </div>
+        </div>
+        <div className={classes.signupBox}>
+          <p>회원가입 하러 가기</p>
+          <ExitToAppIcon className={classes.signupImg} />
+        </div>
       </div>
     </div>
   );
