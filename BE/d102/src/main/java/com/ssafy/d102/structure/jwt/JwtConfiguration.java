86651850db60c1bdae1de9d356ece6e7fc9f7658
박재현 <jhp1276@naver.com>
2023-03-30 13:50:21 +0900

    fix:be jwt config

--- BE/d102/src/main/java/com/ssafy/d102/structure/jwt/JwtConfiguration.java
+++ BE/d102/src/main/java/com/ssafy/d102/structure/jwt/JwtConfiguration.java
@@ -15,7 +15,7 @@ public class JwtConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
                 .order(1)
-                .addPathPatterns(
+                .addPathPatterns("/openvidu"
 //                        "/organization/**", "/worker/**", "/user/**", "/cctv/**", "/membership/**"
                 )   // 탐색 부분 설정
 //                .excludePathPatterns("/user/login", "/user/regist", // 탐색 제외 부분 설정
