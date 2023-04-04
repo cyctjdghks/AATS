998bdda08f89972d09dc38037785e74e0e8d2463
박재현 <jhp1276@naver.com>
2023-04-04 17:39:24 +0900

    feat: be contact add

---
+++ BE/d102/src/main/java/com/ssafy/d102/data/dto/request/ContactDto.java
@@ -0,0 +1,17 @@
+package com.ssafy.d102.data.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactDto {
+
+    private String name;
+    private String number;
+    private String email;
+    private String msg;
+
+}
