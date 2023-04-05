1bc9f57ee3753511626b1c477a33f2997e94426b
강모현 <rkdahgus12@naver.com>
2023-04-05 14:28:59 +0900

    Update people_counting.py

--- Ai_gunzip/people_counting.py
+++ Ai_gunzip/people_counting.py
@@ -3,7 +3,6 @@ import numpy as np
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import base64
-app=qweaedivgnjawieugauwevgjkabkegaulegukgeuk
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
